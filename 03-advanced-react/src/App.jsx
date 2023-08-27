b3acce2b4454aa1026700c89617d6ee825f6b46e
Ahmed Waqar <37536931+UREYPRICE@users.noreply.github.com>
2023-08-27 16:40:09 +0000

    Add files

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,38 @@
-function App() {
-  return (
-    <div className='container'>
-      <h2>Advanced React</h2>
+import { useState, useEffect } from "react";
+import course from "./components/Course";
+const App = () => {
+  console.log(course);
+  return course.map((course) => (
+    <div>
+      <Header course={course} />
+      {course.parts.map((part) => (
+        <Course name={part.name} ex={part.exercises} />
+      ))}
+
+      <Total total={course.parts} />
     </div>
+  ));
+};
+
+const Header = ({ course }) => {
+  console.log(course.name);
+  return <h1>{course.name}</h1>;
+};
+
+const Course = ({ ex, name }) => {
+  return (
+    <>
+      <p>{name}</p>
+      <h8>{ex}</h8>
+    </>
   );
-}
+};
+
+const Total = ({ total }) => {
+  const totalex = total.reduce((i, total) => {
+    return i + total.exercises;
+  }, 0);
+  return <h4>Total of {totalex} exercises</h4>;
+};
 
 export default App;
